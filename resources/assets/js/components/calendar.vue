12a5e398cd8d7628e415e6e2eb71735008f39a31
Brent Ellis <brent.ellis@live.com>
2019-09-06 10:10:25 -0700

    Updated calendar vue component.

--- resources/assets/js/components/calendar.vue
+++ resources/assets/js/components/calendar.vue
@@ -1,3 +1,7 @@
+<style lang='scss'>
+    @import '~@fullcalendar/core/main.css';
+    @import '~@fullcalendar/daygrid/main.css';
+</style>
 <script>  
     import FullCalendar from '@fullcalendar/vue'
     import dayGridPlugin from '@fullcalendar/daygrid'
@@ -15,42 +19,6 @@
             openModal(date){
                 this.$store.dispatch("openModal", {date: date});
             }
-        },
-        mounted: function(){
-            var vm = this;
-            
-            // $(".cal").fullCalendar({
-            //         header: {
-            //             left: 'prev, next today',
-            //             center: 'title',
-            //             right: 'month, agendaWeek, agendaDay, list'
-            //         },
-            //         editable: true,
-            //         droppable: true,
-            //         eventLimit: true,
-            //         weekends: true,
-            //         eventSources: [{
-            //             //Project Due Dates
-            //             url: '/api/projectDueDates',
-            //             color: 'red',
-            //         }],
-            //         navLinks: true,
-            //         eventRender: function (event, element) {
-            //             element.click(function () {
-            //                 console.log("eventRender element.click");
-            //             })
-            //         },
-
-            //         eventClick: function (event, jsEvent, view) {
-            //             console.log(event);
-            //             // vm.openModal();
-            //         },
-
-            //         dayClick: function (date, jsEvent, view) {
-            //             console.log('dayClick');
-            //             // vm.openModal(date);
-            //         }
-            //     });
         },
         components: {
             FullCalendar // make the <FullCalendar> tag available
